4ea92e6406da08e8acc0c2fe01d079955702d045
Hoa Tran <hoatran142@outlook.com>
2021-11-02 15:51:03 +0700

    Creating a connection pool with psycopg2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import psycopg2
 import database
 from models.poll import Poll
 from models.option import Option
-from connections import create_connection
+from connection_pool import pool
 
 
 DATABASE_PROMPT = "Enter the DATABASE_URI value or leave empty to load from .env file: "
@@ -89,8 +89,9 @@ MENU_OPTIONS = {
 
 
 def menu():
-    connection = create_connection()
+    connection = pool.getconn
     database.create_tables(connection)
+    pool.putconn(connection)
 
     while (selection := input(MENU_PROMPT)) != "6":
         try:
--- models/poll.py
+++ models/poll.py
@@ -1,5 +1,5 @@
 from typing import List
-from connections import create_connection
+from connection_pool import pool
 from models.option import Option
 import database
 
@@ -13,9 +13,9 @@ class Poll:
         return f"Poll({self.title!r}, {self.owner!r}, {self.id!r}"   # Poll('title', 'owner', 1)
 
     def save(self):
-        connection = create_connection()
+        connection = pool.getconn()
         new_poll_id = database.create_poll(connection, self.title, self.owner)
-        connection.close()
+        pool.putconn(connection)
         self.id = new_poll_id
 
     def add_option(self, option_text: str):
@@ -23,31 +23,31 @@ class Poll:
 
     @property
     def options(self) -> List[Option]:
-        connection = create_connection()
+        connection = pool.getconn()
         options = database.get_poll_options(connection, self.id)
-        connection.close()
+        pool.putconn(connection)
         return [Option(option[1], option[2], option[0]) for option in options]
 
     @classmethod
     def get(cls, poll_id: int) -> "Poll":
-        connection = create_connection()
+        connection = pool.getconn()
         poll = database.get_poll(connection, poll_id)
-        connection.close()
+        pool.putconn(connection)
         return cls(poll[1], poll[2], poll[0])
 
     @classmethod
     def all(cls) -> List["Poll"]:
-        connection = create_connection()
+        connection = pool.getconn()
         polls = database.get_polls(connection)
-        connection.close()
+        pool.putconn(connection)
         return [cls(poll[1], poll[2], poll[0]) for poll in polls]
 
 
     @classmethod
     def latest(cls) -> "Poll":
-        connection = create_connection()
+        connection = pool.getconn()
         poll = database.get_latest_poll(connection)
-        connection.close()
+        pool.putconn(connection)
         return cls(poll[1], poll[2], poll[0])
 
 
